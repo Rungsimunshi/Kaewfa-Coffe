f9ffeffdfcca8a8869507df9a60dd119991f7e1d
Purit2405 <timi92275@gmail.com>
2025-09-16 09:50:39 +0700

    first commit

---
+++ app/src/main/java/com/rungsimun/kaewfacoffee/PointsActivity.java
@@ -0,0 +1,6 @@
+package com.rungsimun.kaewfacoffee;
+
+import android.app.Activity;
+
+public class PointsActivity extends Activity {
+}
---
+++ app/src/main/java/com/rungsimun/kaewfacoffee/RedemptionActivity.java
@@ -0,0 +1,6 @@
+package com.rungsimun.kaewfacoffee;
+
+import android.app.Activity;
+
+public class RedemptionActivity extends Activity {
+}
